dc59c3708f859ca120b554daa892dae032ad46ab
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-06-24 16:11:37 +0100

    removed return statement

--- src/main/scala/WaspPackage/Commander.scala
+++ src/main/scala/WaspPackage/Commander.scala
@@ -16,57 +16,60 @@ object Commander {
     def waspRecursion(currentWasps: Wasps): Boolean = {
       if (currentWasps.waspList.isEmpty) {
         println("Game Over")
-        return false
-      }
-
-      val random = new Random()
-      val targetIndex = random.nextInt(currentWasps.waspList.length)
-      var targetWasp = currentWasps.waspList(targetIndex)
-
-      println(s"\nA random Wasp has been selected ${targetWasp.rank}\n Would you like to fire: \n f:fire")
-      val choice = StdIn.readLine()
-
-      if (choice == "f") {
-        println(s"Firing at wasp with rank ${targetWasp.rank} who has ${targetWasp.lives} lives!\n")
-
-        targetWasp = targetWasp.copy(lives = targetWasp.lives - targetWasp.deductable)
-
-        if (targetWasp.lives <= 0) {
-          // Remove the dead wasp from currentWasps
-          println(s"${targetWasp.rank} has been eliminated!")
-          val newWaspsList = currentWasps.waspList.filterNot(_.id == targetWasp.id)
-          val newWasps = new Wasps(newWaspsList)
-          println(s"${targetWasp.rank} remaining lives: ${targetWasp.lives}")
-          println("-".repeat(100))
-          newWasps.waspList.foreach(wasp => println(s"Rank: ${wasp.rank} lives Remaining: ${wasp.lives}"))
-          newWasps.waspList.find(wasp => wasp.rank == "queenWasp") match {
-            case None =>
-              println("The Queen is dead!")
-              return false
-            case _ =>
-          }
-          println("-".repeat(100))
-          waspRecursion(newWasps)
-        } else {
-          // Update the currentWasps list with the modified targetWasp
-          val newWaspsList = currentWasps.waspList.updated(targetIndex, targetWasp)
-          val newWasps = new Wasps(newWaspsList)
-          println(s"${targetWasp.rank} remaining lives: ${targetWasp.lives}")
-          println("All Wasps")
-          newWasps.waspList.foreach(wasp => println(s"Rank: ${wasp.rank} lives Remaining: ${wasp.lives}"))
-          waspRecursion(newWasps)
-        }
-      } else {
-        println("Invalid choice, exiting.")
         false
+      } else {
+        val random = new Random()
+        val targetIndex = random.nextInt(currentWasps.waspList.length)
+        var targetWasp = currentWasps.waspList(targetIndex)
+
+        println(s"\nA random Wasp has been selected ${targetWasp.rank}\n Would you like to fire: \n f:fire")
+        val choice = StdIn.readLine()
+
+        if (choice == "f") {
+          println(s"Firing at wasp with rank ${targetWasp.rank} who has ${targetWasp.lives} lives!\n")
+
+          // Used the copy method on case class to override the lives property
+          targetWasp = targetWasp.copy(lives = targetWasp.lives - targetWasp.deductable)
+
+          if (targetWasp.lives <= 0) {
+            // Remove the dead wasp from currentWasps
+            println(s"${targetWasp.rank} has been eliminated!")
+            val newWaspsList = currentWasps.waspList.filterNot(_.id == targetWasp.id)
+            // New List is created (instantiated) with the dead wasp removed
+            val newWasps = new Wasps(newWaspsList)
+            println(s"${targetWasp.rank} remaining lives: ${targetWasp.lives}")
+            println("-" * 100)
+            newWasps.waspList.foreach(wasp => println(s"Rank: ${wasp.rank} lives Remaining: ${wasp.lives}"))
+            // .find returns type Option[A]
+            newWasps.waspList.find(wasp => wasp.rank == "queenWasp") match {
+              case None =>
+                println("The Queen is dead!")
+                false
+              case Some(_) => // Changed: case _ replaced with case Some(_)
+                println("-" * 100)
+                waspRecursion(newWasps) // If its not a queen then the recursion is triggered
+            }
+          } else {
+            // Update the currentWasps list with the modified targetWasp
+            val newWaspsList = currentWasps.waspList.updated(targetIndex, targetWasp)
+            val newWasps = new Wasps(newWaspsList)
+            println(s"${targetWasp.rank} remaining lives: ${targetWasp.lives}")
+            println("All Wasps")
+            newWasps.waspList.foreach(wasp => println(s"Rank: ${wasp.rank} lives Remaining: ${wasp.lives}"))
+            waspRecursion(newWasps) // Changed: no return keyword, just the recursive call
+          }
+        } else {
+          println("Invalid choice, exiting.")
+          false
+        }
       }
     }
 
+
     def fire(): Unit = {
       val continueLoop = waspRecursion(wasps)
-
+      println("-".repeat(100))
       if (!continueLoop) {
-        println("Nice Job!!")
         println("Would you like to play again?\n p: play again")
         val option = StdIn.readLine()
         if (option == "p") {
--- src/main/scala/WaspPackage/WaspFactory.scala
+++ src/main/scala/WaspPackage/WaspFactory.scala
@@ -5,18 +5,18 @@ import java.util.UUID
 object WaspFactory {
   def generateWorkerWasps(): Wasps = {
     val numberOfWasps = 5
-    val wasps = (1 to numberOfWasps).map(_ => new Wasp(UUID.randomUUID(), "workerWasps", 68, 10)).toList
+    val wasps = (1 to numberOfWasps).map(_ => new Wasp(UUID.randomUUID(), "workerWasps", 68, 68)).toList
     new Wasps(wasps)
   }
 
   def generateDroneWasps(): Wasps = {
     val numberOfWasps = 8
-    val wasps = (1 to numberOfWasps).map(_ => new Wasp(UUID.randomUUID(), "droneWasps", 60 , 12)).toList
+    val wasps = (1 to numberOfWasps).map(_ => new Wasp(UUID.randomUUID(), "droneWasps", 60 , 60)).toList
     new Wasps(wasps)
   }
 
   def generateQueenWasp(): Wasps = {
-    val queenWasp = List(new Wasp(UUID.randomUUID(), "queenWasp", 80, 7))
+    val queenWasp = List(new Wasp(UUID.randomUUID(), "queenWasp", 80, 80))
     new Wasps(queenWasp)
   }
 }
